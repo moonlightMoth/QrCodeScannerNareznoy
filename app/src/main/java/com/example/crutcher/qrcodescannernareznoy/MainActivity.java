4572ea0260e31ed9a57417ddacf97e1715d47072
moonlightMoth <ghjpa@inbox.ru>
2018-04-27 01:26:49 +0400

    Initial commit

---
+++ app/src/main/java/com/example/crutcher/qrcodescannernareznoy/MainActivity.java
@@ -0,0 +1,63 @@
+package com.example.crutcher.qrcodescannernareznoy;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mAuth = FirebaseAuth.getInstance();
+
+        EditText email22 = findViewById(R.id.email);
+        EditText password22 = findViewById(R.id.password);
+
+        String email = email22.getText().toString();
+        String password = password22.getText().toString();
+
+        if(email == "admin" && password == "nareznoygh")
+        {
+            mAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if(task.isSuccessful()) {
+                        Toast.makeText(MainActivity.this, "Aвторизация успешна", Toast.LENGTH_SHORT).show();
+
+
+                        Intent intent= new Intent(MainActivity.this, ScanActivity.class);
+                        MainActivity.this.startActivity(intent);
+                        MainActivity.this.finish();
+
+                    }else
+                        Toast.makeText(MainActivity.this, "Aвторизация провалена", Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+        else
+            Toast.makeText(MainActivity.this, "Aвторизация провалена", Toast.LENGTH_SHORT).show();
+
+
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+    }
+}
---
+++ app/src/main/java/com/example/crutcher/qrcodescannernareznoy/ScanActivity.java
@@ -0,0 +1,14 @@
+package com.example.crutcher.qrcodescannernareznoy;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+public class ScanActivity extends AppCompatActivity {
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scan);
+    }
+}
