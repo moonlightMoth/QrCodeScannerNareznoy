4286a1852292dbeac8c5e464720f038dfda99f01
moonlightMoth <ghjpa@inbox.ru>
2018-04-27 23:48:33 +0400

    рабочий скелетик + вывод имэйла в строку и поинтов на кнопку

--- app/src/main/java/com/example/crutcher/qrcodescannernareznoy/ScanActivity.java
+++ app/src/main/java/com/example/crutcher/qrcodescannernareznoy/ScanActivity.java
@@ -9,6 +9,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 
 import com.dlazaro66.qrcodereaderview.QRCodeReaderView;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 
 public class ScanActivity extends AppCompatActivity {
     @Override
@@ -31,11 +37,48 @@ public class ScanActivity extends AppCompatActivity {
         // Use this function to set front camera preview
         qrCodeReaderView.setBackCamera();
 
+        final FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
+
+
         final TextView textView = findViewById(R.id.text);
+        final TextView button = findViewById(R.id.button);
         qrCodeReaderView.setOnQRCodeReadListener(new QRCodeReaderView.OnQRCodeReadListener() {
             @Override
             public void onQRCodeRead(String text, PointF[] points) {
-                textView.setText(text);
+
+                try {
+
+                    DatabaseReference databaseReference = firebaseDatabase.getReference(text);
+
+                    databaseReference.child("gmail").addListenerForSingleValueEvent(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(DataSnapshot dataSnapshot) {
+                            textView.setText(dataSnapshot.getValue().toString());
+                        }
+
+                        @Override
+                        public void onCancelled(DatabaseError databaseError) {
+
+                        }
+                    });
+
+                    databaseReference.child("points").addListenerForSingleValueEvent(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(DataSnapshot dataSnapshot) {
+                            button.setText(dataSnapshot.getValue().toString());
+                        }
+
+                        @Override
+                        public void onCancelled(DatabaseError databaseError) {
+
+                        }
+                    });
+                }
+                catch (Exception e)
+                {
+                    textView.setText("no such user");
+                }
+
             }
         });
     }
