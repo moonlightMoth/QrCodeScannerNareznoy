8099c1b17982bb369a55d464cdec9ef444a5d573
moonlightMoth <ghjpa@inbox.ru>
2018-04-27 21:53:05 +0400

    рабочий скелетик

--- app/src/main/java/com/example/crutcher/qrcodescannernareznoy/MainActivity.java
+++ app/src/main/java/com/example/crutcher/qrcodescannernareznoy/MainActivity.java
@@ -5,7 +5,10 @@ import android.content.SharedPreferences;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.AppCompatButton;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -24,32 +27,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         mAuth = FirebaseAuth.getInstance();
 
-        EditText email22 = findViewById(R.id.email);
-        EditText password22 = findViewById(R.id.password);
-
-        String email = email22.getText().toString();
-        String password = password22.getText().toString();
-
-        if(email == "admin" && password == "nareznoygh")
-        {
-            mAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                @Override
-                public void onComplete(@NonNull Task<AuthResult> task) {
-                    if(task.isSuccessful()) {
-                        Toast.makeText(MainActivity.this, "Aвторизация успешна", Toast.LENGTH_SHORT).show();
-
-
-                        Intent intent= new Intent(MainActivity.this, ScanActivity.class);
-                        MainActivity.this.startActivity(intent);
-                        MainActivity.this.finish();
-
-                    }else
-                        Toast.makeText(MainActivity.this, "Aвторизация провалена", Toast.LENGTH_SHORT).show();
-                }
-            });
-        }
-        else
-            Toast.makeText(MainActivity.this, "Aвторизация провалена", Toast.LENGTH_SHORT).show();
+        AppCompatButton button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                singin();
+            }
+        });
 
 
     }
@@ -59,5 +43,39 @@ public class MainActivity extends AppCompatActivity {
         super.onStart();
         // Check if user is signed in (non-null) and update UI accordingly.
         FirebaseUser currentUser = mAuth.getCurrentUser();
+    }
+
+
+
+    public void singin()
+    {
+        EditText email22 = findViewById(R.id.email);
+        EditText password22 = findViewById(R.id.password);
+
+        String email = email22.getText().toString();
+        String password = password22.getText().toString();
+
+        if(email.equals( "admin@narez.ru" )&& password.equals( "nareznoygh"))
+        {
+            Toast.makeText(MainActivity.this, "Aвторизация успешна1", Toast.LENGTH_SHORT).show();
+            mAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if(task.isSuccessful()) {
+                        Toast.makeText(MainActivity.this, "Aвторизация успешна2", Toast.LENGTH_SHORT).show();
+
+
+                        Intent intent= new Intent(MainActivity.this, ScanActivity.class);
+                        MainActivity.this.startActivity(intent);
+                        MainActivity.this.finish();
+
+                    }else
+                        Toast.makeText(MainActivity.this, "Aвторизация провалена2", Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+        else {
+            Toast.makeText(MainActivity.this, "Aвторизация провалена1 " + email + " " + password, Toast.LENGTH_SHORT).show();
+        }
     }
 }
--- app/src/main/java/com/example/crutcher/qrcodescannernareznoy/ScanActivity.java
+++ app/src/main/java/com/example/crutcher/qrcodescannernareznoy/ScanActivity.java
@@ -1,14 +1,42 @@
 package com.example.crutcher.qrcodescannernareznoy;
 
+import android.Manifest;
+import android.graphics.PointF;
 import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import com.dlazaro66.qrcodereaderview.QRCodeReaderView;
 
 public class ScanActivity extends AppCompatActivity {
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scan);
+
+        requestPermissions(new String[]{Manifest.permission.CAMERA}, 1);
+
+        QRCodeReaderView qrCodeReaderView = findViewById(R.id.qrReader);
+
+        qrCodeReaderView.setQRDecodingEnabled(true);
+
+        // Use this function to change the autofocus interval (default is 5 secs)
+        qrCodeReaderView.setAutofocusInterval(2000L);
+
+        // Use this function to enable/disable Torch
+        qrCodeReaderView.setTorchEnabled(false);
+
+        // Use this function to set front camera preview
+        qrCodeReaderView.setBackCamera();
+
+        final TextView textView = findViewById(R.id.text);
+        qrCodeReaderView.setOnQRCodeReadListener(new QRCodeReaderView.OnQRCodeReadListener() {
+            @Override
+            public void onQRCodeRead(String text, PointF[] points) {
+                textView.setText(text);
+            }
+        });
     }
 }
